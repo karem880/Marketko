ebd799cc83e7acb4430dec568e003bb84b191350
karem880 <elalamymee@gmail.com>
2023-10-21 21:09:25 +0200

    h

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import NavBar from '../component/NavBar';
 import axios from 'axios';
-import { MdShoppingCart } from 'react-icons/md';
+import { MdshoppingCart } from 'react-icons/md';
 import ComplexTouchGallery from './slider';
 
 
@@ -24,10 +24,10 @@ function Home() {
         <h2 className='text-green-600 mt-2 text-lg font-extrabold text-center'>Explore our sections by clicking the button below.</h2>
 
         <p className='text-gray-700 text-base mt-4'>Discover the freshest produce and more!</p>
-        <NavLink to={'shop'} className='w-full md:w-[70%] text-xl font-bold p-3 bg-green-600 text-white flex items-center justify-center h-[50px] rounded-lg mt-6 hover:bg-green-700 transition duration-300 ease-in-out'>Explore Market</NavLink>
+        <NavLink to={'/shop'} className='w-full md:w-[70%] text-xl font-bold p-3 bg-green-600 text-white flex items-center justify-center h-[50px] rounded-lg mt-6 hover:bg-green-700 transition duration-300 ease-in-out'>Explore Market</NavLink>
     </div>
 
-    <NavLink to={'shop'} className='flex items-center mx-auto  justify-center h-full flex-col md:flex-row w-full md:w-[50%] mt-10'>
+    <NavLink to={'/shop'} className='flex items-center mx-auto  justify-center h-full flex-col md:flex-row w-full md:w-[50%] mt-10'>
         <div className='flex h-[70%] flex-col md:flex-row overflow-hidden gap-3 rounded-2xl'>
             <div className='w-full md:w-[50%] h-full flex flex-col gap-3'>
                 <img src="./assets/vegtablesheader.webp" className='h-[300px] rounded-lg object-cover w-full md:w-[300px]' alt="" />
@@ -51,7 +51,7 @@ function Home() {
       {/* Content for the first div */}
       <p>Discover the Hidden Treasures</p>
       <p className="text-xl font-semibold mt-4">Explore our exquisite collection of fresh and premium products.</p>
-      <NavLink to={'shop'} className='w-[70%] md:w-[50%] text-xl font-bold p-3 bg-green-600 text-white flex items-center justify-center h-[50px] rounded-lg mt-6 hover:bg-green-700 transition duration-300 ease-in-out'>Explore Market</NavLink>
+      <NavLink to={'/shop'} className='w-[70%] md:w-[50%] text-xl font-bold p-3 bg-green-600 text-white flex items-center justify-center h-[50px] rounded-lg mt-6 hover:bg-green-700 transition duration-300 ease-in-out'>Explore Market</NavLink>
 
     </div>
   </div>
@@ -103,11 +103,11 @@ function Home() {
 <section className='bg-gradient-to-r from-green-500 to-green-300 py-24 mt-32 text-center text-white'>
   <div className='container mx-auto flex flex-col items-center'>
     <div className='w-full flex items-center justify-center text-white text-[100px] py-10'>
-      <MdShoppingCart/>
+      <MdshoppingCart/>
     </div>
     <h2 className='text-4xl font-extrabold mb-6'>Discover Something Extraordinary</h2>
     <p className='text-2xl mb-10'>Unleash your imagination and embark on a journey of flavor and delight with our unique selection of products.</p>
-    <Link to='/shop' className='bg-gradient-to-r from-green-500 to-green-300 hover:bg-green-600 text-white text-lg font-semibold px-8 py-4 rounded-full transition duration-300 ease-in-out'>Explore Now</Link>
+    <Link to='//shop' className='bg-gradient-to-r from-green-500 to-green-300 hover:bg-green-600 text-white text-lg font-semibold px-8 py-4 rounded-full transition duration-300 ease-in-out'>Explore Now</Link>
   </div>
 </section>
 
